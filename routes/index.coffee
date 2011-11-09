be3e1dc565dc1d89db219ffb229c701d311d250a
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2011-11-08 22:44:52 -0600

    Ahora muestra el titulo del empleo y el sueldo que se ofrece

--- routes/index.coffee
+++ routes/index.coffee
@@ -1,59 +1,27 @@
 
-request = require 'request' # facilita el cargar "sitios" 
-jsdom   = require 'jsdom' # esto no es js de navegador!!
+mongodb = require 'mongodb'
+
+Db = mongodb.Db
+Connection = mongodb.Connection
+Server = mongodb.Server
+
+DB_HOST = process.env.DB_HOST or '127.0.0.1'
+DB_PORT = process.env.DB_PORT or 5837
+DB_USER = process.env.DB_USER or 'root'
+DB_PASSWD = process.env.DB_PASSWD or ''
 
 # pagina inicial (aka home)
-exports.index = (req, res) ->
-	# solo me interesa el foro de HTML5
-	request { 'uri' : 'http://www.forosdelweb.com/f175/' },
-		(err, respuesta, cuerpo) ->
-			# transformarlo magicamente en DOM con jQuery
-			jsdom.env {html: cuerpo, scripts: ['http://code.jquery.com/jquery-1.6.min.js']},
-				(err, window) ->
-					$ = window.jQuery
-					items = [] # aqui van los datos de los temas
-
-					# selector (td)
-					$('.alt1.tdtitle').each ->
-						$self = $ this
-						$contenedor = $self.children 'div'
-						$a = $contenedor.children 'a'
-
-						# titulo, url y autor por ahora
-						items.push
-							titulo: $a.text()
-							href: $a.attr('href').split('f175')[1]
-							autor: $self.children('div + div').text()
-					
-					# mostrarlo bonito en la plantilla
+exports.index = (req, res) ->	
+	db = new Db('fdw', new Server(DB_HOST, DB_PORT, {}), native_parser: false )
+			
+	# abrir la conexion a la base de datos
+	db.open (err, db) ->
+		db.authenticate DB_USER, DB_PASSWD, (err) ->
+			# crear la coleccion de empleos
+			db.collection 'empleos', (err, collection) ->
+				collection.find({}, {}).toArray (err, empleos) ->
 					res.render 'index',
-						title: 'HTML5 - Foros del Web'
-						items: items
+						title: 'Empleo - Foros del Web'
+						empleos: empleos
 
-# pagina de tema (aka /super-tema-padre-55)
-exports.tema = (req, res) ->
-	# como root es el foro de html5
-	request {'uri':"http://www.forosdelweb.com/f175/#{req.params.tema}"},
-		(err, respuesta, cuerpo) ->
-			# transformarlo magicamente en DOM con jQuery
-			jsdom.env { html: cuerpo, scripts: ['http://code.jquery.com/jquery-1.6.min.js']},
-				(err, window) ->
-					$ = window.jQuery
-					# contenedor de [post, comentarios]					
-					$post = $ '#posts'
-					$todos = $post.find('.post_content')
-					comentarios = [] # guardar aqui los comentarios
-
-					# tomar los comentarios
-					$todos.slice(1).each (i, el) -> comentarios.push $(el).html()
-
-					# propiedades del tema: titulo, texto y comentarios(array)
-					item = 
-						titulo: $post.find('.alt1 .smallfont').first().text()
-						contenido: $todos.first().html()
-						comentarios: comentarios
-					
-					# enviar con la plantilla
-					res.render 'tema',
-						title: "#{item.titulo} - HTML5 - Foros del Web"
-						item: item
+				
--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,27 @@
 (function() {
-  var jsdom, request;
-  request = require('request');
-  jsdom = require('jsdom');
+  var Connection, DB_HOST, DB_PASSWD, DB_PORT, DB_USER, Db, Server, mongodb;
+  mongodb = require('mongodb');
+  Db = mongodb.Db;
+  Connection = mongodb.Connection;
+  Server = mongodb.Server;
+  DB_HOST = process.env.DB_HOST || '127.0.0.1';
+  DB_PORT = process.env.DB_PORT || 5837;
+  DB_USER = process.env.DB_USER || 'root';
+  DB_PASSWD = process.env.DB_PASSWD || '';
   exports.index = function(req, res) {
-    return request({
-      'uri': 'http://www.forosdelweb.com/f175/'
-    }, function(err, respuesta, cuerpo) {
-      return jsdom.env({
-        html: cuerpo,
-        scripts: ['http://code.jquery.com/jquery-1.6.min.js']
-      }, function(err, window) {
-        var $, items;
-        $ = window.jQuery;
-        items = [];
-        $('.alt1.tdtitle').each(function() {
-          var $a, $contenedor, $self;
-          $self = $(this);
-          $contenedor = $self.children('div');
-          $a = $contenedor.children('a');
-          return items.push({
-            titulo: $a.text(),
-            href: $a.attr('href').split('f175')[1],
-            autor: $self.children('div + div').text()
-          });
-        });
-        return res.render('index', {
-          title: 'HTML5 - Foros del Web',
-          items: items
-        });
-      });
+    var db;
+    db = new Db('fdw', new Server(DB_HOST, DB_PORT, {}), {
+      native_parser: false
     });
-  };
-  exports.tema = function(req, res) {
-    return request({
-      'uri': "http://www.forosdelweb.com/f175/" + req.params.tema
-    }, function(err, respuesta, cuerpo) {
-      return jsdom.env({
-        html: cuerpo,
-        scripts: ['http://code.jquery.com/jquery-1.6.min.js']
-      }, function(err, window) {
-        var $, $post, $todos, comentarios, item;
-        $ = window.jQuery;
-        $post = $('#posts');
-        $todos = $post.find('.post_content');
-        comentarios = [];
-        $todos.slice(1).each(function(i, el) {
-          return comentarios.push($(el).html());
-        });
-        item = {
-          titulo: $post.find('.alt1 .smallfont').first().text(),
-          contenido: $todos.first().html(),
-          comentarios: comentarios
-        };
-        return res.render('tema', {
-          title: "" + item.titulo + " - HTML5 - Foros del Web",
-          item: item
+    return db.open(function(err, db) {
+      return db.authenticate(DB_USER, DB_PASSWD, function(err) {
+        return db.collection('empleos', function(err, collection) {
+          return collection.find({}, {}).toArray(function(err, empleos) {
+            return res.render('index', {
+              title: 'Empleo - Foros del Web',
+              empleos: empleos
+            });
+          });
         });
       });
     });
